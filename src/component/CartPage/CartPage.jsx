8b89339c14b5f436cb36c9e5e9203fefa32df19e
Eman Masoud <114053062+E1442020@users.noreply.github.com>
2023-02-27 16:55:26 +0200

    cart

--- src/component/CartPage/CartPage.jsx
+++ src/component/CartPage/CartPage.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
+import { Loader } from "@mantine/core";
 import { Link } from "react-router-dom";
 import "./CartPage.css";
 import CartProduct from "./CartProduct";
 export default function CartPage() {
   const [cartProducts, setCartProducts] = useState([]);
   const [totalPrice, setTotalPrice] = useState("");
+  const [loading, setLoading] = useState(true);
   //get Product from localStorage
 
   const cartProductFromLocalStorage = () => {
@@ -19,122 +21,129 @@ export default function CartPage() {
   const removeProduct = (id) => {
     const swalWithBootstrapButtons = Swal.mixin({
       customClass: {
-        confirmButton: 'btn btn-success',
-        cancelButton: 'btn btn-danger'
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger",
       },
-      buttonsStyling: true
-    })
-    
-    swalWithBootstrapButtons.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Yes, delete it!',
-      cancelButtonText: 'No, cancel!',
-      reverseButtons: true
-    }).then((result) => {
-      if (result.isConfirmed) {
-        let tempCartProduct = [...cartProducts];
-    
-    tempCartProduct.map((product, index) => {
-      if (product.id === id) {
-        tempCartProduct.splice(index, 1);
-        getTotalPrice(tempCartProduct)
-        setCartProducts(tempCartProduct);
-        localStorage.setItem("cartProducts", JSON.stringify(tempCartProduct));
-      }
+      buttonsStyling: true,
     });
-        swalWithBootstrapButtons.fire(
-          'Deleted!',
-          'Your product deleted.',
-          'success'
-        )
-      } else if (
-        /* Read more about handling dismissals below */
-        result.dismiss === Swal.DismissReason.cancel
-      ) {
-        swalWithBootstrapButtons.fire(
-          'Cancelled',
-          'Your product safe :)',
-          'error'
-        )
-      }
-    })
-    
+
+    swalWithBootstrapButtons
+      .fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          let tempCartProduct = [...cartProducts];
+
+          tempCartProduct.map((product, index) => {
+            if (product.id === id) {
+              tempCartProduct.splice(index, 1);
+              getTotalPrice(tempCartProduct);
+              setCartProducts(tempCartProduct);
+              localStorage.setItem(
+                "cartProducts",
+                JSON.stringify(tempCartProduct)
+              );
+            }
+          });
+          swalWithBootstrapButtons.fire(
+            "Deleted!",
+            "Your product deleted.",
+            "success"
+          );
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            "Cancelled",
+            "Your product safe :)",
+            "error"
+          );
+        }
+      });
   };
 
   const getTotalPrice = (tempArr) => {
-    
     let sum = 0;
 
     if (tempArr.length == 0) {
-      sum=sum;}
-      
-    // } else {
-      tempArr.map((product) => {
-        sum += parseInt(product.price * product.quantity);
-        
-        
-      });
+      sum = sum;
+    }
 
-      // console.log(sum);
-    
+    // } else {
+    tempArr.map((product) => {
+      sum += parseInt(product.price * product.quantity);
+    });
+
+    // console.log(sum);
+
     setTotalPrice(sum);
   };
 
-  const updateLocal = (quantity,id) => {
+  const updateLocal = (quantity, id) => {
     let tempCartProduct = [...cartProducts];
     tempCartProduct.map((product) => {
-     
-     
       if (product.id === id) {
-       
         product.quantity = quantity;
         setCartProducts(tempCartProduct);
         localStorage.setItem("cartProducts", JSON.stringify(tempCartProduct));
       }
-      });
-   
+    });
   };
 
   useEffect(() => {
     const tempArr = cartProductFromLocalStorage();
     setCartProducts(tempArr);
-    // console.log(tempArr);
     getTotalPrice(tempArr);
+    if (tempArr) {
+      setLoading(false);
+    }
   }, []);
   // localStorage.clear()
+
   return (
     <>
       <div className="cart-page-container">
-        
-          {cartProducts.length==0?<h3 className="no-product">No Product Available...Add Some!!</h3>:<>
-       
-        {cartProducts.map((cartProduct) => {
-          return (
-            <CartProduct
-              name={cartProduct.name}
-              price={cartProduct.price}
-              quantity={cartProduct.quantity}
-              img={cartProduct.img}
-              key={cartProduct.id}
-              id={cartProduct.id}
-              arr={cartProducts}
-              getTotal={getTotalPrice}
-              remove={removeProduct}
-              updateQuantity={updateLocal}
-            />
-          );
-        })}</>}
+      {loading ?<div className="loading"><Loader color="cyan" /></div>:<>
+
+        {cartProducts.length > 0 ? (
+          <>
+
+            {cartProducts.map((cartProduct) => {
+              return (
+                <CartProduct
+                  name={cartProduct.name}
+                  price={cartProduct.price}
+                  quantity={cartProduct.quantity}
+                  img={cartProduct.img}
+                  key={cartProduct.id}
+                  id={cartProduct.id}
+                  arr={cartProducts}
+                  getTotal={getTotalPrice}
+                  remove={removeProduct}
+                  updateQuantity={updateLocal}
+                />
+              );
+            })}
+          </>
+        ) : (
+          <h3 className="no-product">No Product Available...Add Some!!</h3>
+        )}
 
         <div className="cart-page-footer">
           <h2>Total Price:${totalPrice}</h2>
           <button>
             <Link to="/">continue to shopping</Link>
           </button>
-        </div>
-      </div>
+        </div></>}</div>
+      
     </>
   );
 }
